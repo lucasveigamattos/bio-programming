dfc8aa656c54e51a7961485367102d99514f26c9
Lucas Veiga Mattos <lucasveigamattos@gmail.com>
2024-08-24 17:57:19 -0300

    Handling arguments.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
-int main() {
+#include <iostream>
+
+using namespace std;
+
+bool HandleArguments(int argc, char* argv[]) {
+    if (argc != 3) {
+        cout << "> Need to specify the input and output file." << endl;
+        return 1;
+    }
+}
+
+int main(int argc, char* argv[]) {
     return 0;
 }
